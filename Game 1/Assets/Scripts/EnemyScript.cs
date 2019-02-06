871cd296bad37abc01aea85708f83c3fd88933b5
Fernando <A01630401@itesm.mx>
2019-02-06 16:22:40 -0600

    Game 1 first try

---
+++ Game 1/Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyScript : MonoBehaviour
+{
+    private int flag = 1;
+    public Text score;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.Translate(speed * Time.deltaTime * flag, 0, 0);
+
+        //print(transform.position.x);
+        //transform.position.x;
+        //transform.Translate();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        flag *= -1;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.name == "Player 1") { 
+            int score1 = int.Parse(score.text) - 5;
+            score.text = "" + score1;
+        }
+    }
+}
---
+++ Game 1/Assets/Scripts/Player1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player1 : MonoBehaviour
+{
+    public Text score;
+    public int speed;
+    public EnemyScript enemy;
+    private Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(enemy.speed);
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+
+        transform.Translate(5 * x * Time.deltaTime, 0, 5 * z * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.transform.name);
+        if (other.transform.name == "Goal")
+        {
+            int score1 = int.Parse(score.text) + 50;
+            score.text = "" + score1;
+            enemy.speed += 5;
+            transform.SetPositionAndRotation(position, transform.rotation);
+            
+        }
+    }
+}
