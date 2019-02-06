0f60c531f0a332b381f88a368413aecf2d586e34
Fernando <A01630401@itesm.mx>
2019-02-06 13:38:28 -0600

    again classwithphysics

---
+++ ClassWithPhysics/Assets/Scripts/Projectile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        print(transform.up);
+        rb.AddForce(transform.up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
