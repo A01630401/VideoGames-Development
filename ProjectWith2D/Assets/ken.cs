94c31303c9c4c41302c679f3986172b4a7e62514
Fernando <A01630401@itesm.mx>
2019-02-15 12:45:37 -0600

    2D game 15-02-19

---
+++ ProjectWith2D/Assets/ken.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ken : MonoBehaviour
+{
+
+    private Animator a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        transform.Translate(h * 5 * Time.deltaTime, 0, 0);
+        a.SetFloat("moving", h);
+    }
+}
