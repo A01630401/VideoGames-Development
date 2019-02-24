5dbc5b3f747f261635c4f4bcf934f306ed553d63
Fernando <A01630401@itesm.mx>
2019-02-24 11:53:31 -0600

    new project

---
+++ Project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    private GameObject followPoint;
+    private Vector3 followPointPos;
+    private float speed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        followPoint = GameObject.Find("followPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        followPointPos = new Vector3(followPoint.transform.position.x, followPoint.transform.position.y, followPoint.transform.position.z);
+
+        transform.position = Vector3.MoveTowards(transform.position, followPointPos, speed * Time.deltaTime);
+    }
+}
---
+++ Project/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float timer = 0.5f;
+    public GameObject wayPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float j = Input.GetAxis("Horizontal");
+        float h = Input.GetAxis("Vertical");
+        transform.Translate(j * 10 * Time.deltaTime, 0, h * 10 * Time.deltaTime);
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0)
+        {
+            //The position of the waypoint will update to the player's position
+            UpdatePosition();
+            timer = 0.5f;
+        }
+    }
+
+    void UpdatePosition()
+    {
+        //The wayPoint's position will now be the player's current position.
+        wayPoint.transform.position = transform.position;
+    }
+}
