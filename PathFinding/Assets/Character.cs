41a21fb14f70c5e19546cca51cda6e8f8362e153
Fernando <A01630401@itesm.mx>
2019-03-05 12:45:14 -0600

    following objects

---
+++ PathFinding/Assets/Character.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    // stalker
+    public Waypoint[] path;
+    public float threshold;
+
+    private int current;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        current = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(path[current].transform);
+        transform.Translate(transform.forward * Time.deltaTime * 2, Space.World);
+    }
+
+    // We need yo check distance on a regulat bassis with the least
+    // amount of overhead
+
+    // coroutine!
+    IEnumerator CheckDistance()
+    {
+        while (true)
+        {
+            // check distance
+            float d = Vector3.Distance(transform.position, path[current].transform.position);
+
+            if(d < threshold)
+            {
+                current++;
+                current %= path.Length;
+            }
+
+            // wait a little
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+}
