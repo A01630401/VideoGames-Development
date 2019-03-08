b5f4383932384a6c932e3680fdae7dc3317525b9
Fernando <A01630401@itesm.mx>
2019-03-08 12:41:47 -0600

    follow mouse

--- PathFinding/Assets/Character.cs
+++ PathFinding/Assets/Character.cs
@@ -8,6 +8,8 @@ public class Character : MonoBehaviour
     // stalker
     public Waypoint[] path;
     public float threshold;
+    private Coroutine coroutine;
+    private IEnumerator enumerator;
 
     private int current;
 
@@ -15,6 +17,8 @@ public class Character : MonoBehaviour
     void Start()
     {
         current = 0;
+        coroutine = StartCoroutine(CheckDistance());
+        //enumerator = CheckDistance();
     }
 
     // Update is called once per frame
@@ -22,6 +26,14 @@ public class Character : MonoBehaviour
     {
         transform.LookAt(path[current].transform);
         transform.Translate(transform.forward * Time.deltaTime * 2, Space.World);
+
+        if (Input.GetKeyUp(KeyCode.Return))
+        {
+            //StopCoroutine(enumerator);
+            StopCoroutine(coroutine);
+            //StopAllCoroutines();
+
+        }
     }
 
     // We need yo check distance on a regulat bassis with the least
@@ -42,7 +54,51 @@ public class Character : MonoBehaviour
             }
 
             // wait a little
-            yield return new WaitForSeconds(0.2f);
+            yield return new WaitForSeconds(0.1f);
         }
+    }
+
+    private void OnMouseDown()
+    {
+        // clicked over the object
+        print("MOUSE DOWN");
+    }
+
+    private void OnMouseEnter()
+    {
+        // in this frame the pointer is over the object
+        // on the previous it wasn't
+        print("MOUSE ENTER");
+    }
+
+    private void OnMouseDrag()
+    {
+        // clicked on an object and moved the mouse
+        print("MOUSE DRAG");
+    }
+
+    private void OnMouseExit()
+    {
+        //pointer was over the object and now is outside
+        print("MOUSE EXIT");
+    }
+
+    private void OnMouseOver()
+    {
+        // while the pointer is over the object
+        // after enter
+        print("MOUSE OVER");
+    }
+
+    private void OnMouseUp()
+    {
+        // release button over object
+        print("MOUSE UP");
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        // behaves as a GUI button
+        print("MOUSE UP AS BUTTON");
     }
 }
---
+++ PathFinding/Assets/Reycaster.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reycaster : MonoBehaviour
+{
+    private Ray ray;
+    public Transform observer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // raycasting
+        // shooting a ray in (or into) the world
+        // checking where it hit (if it did)
+
+        // get a ray!
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit))
+        {
+            print(hit.transform);
+            print(hit.point );
+            observer.LookAt(hit.point);
+        }
+        else
+        {
+            print("MISS");
+        } 
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawRay(ray);
+    }
+}
