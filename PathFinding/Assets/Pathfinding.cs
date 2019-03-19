fc0d7eb46f0931239686f733a15cd8478b6cda5f
Fernando <A01630401@itesm.mx>
2019-03-19 12:44:52 -0600

    paths with graphs

---
+++ PathFinding/Assets/Pathfinding.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding {
+
+    // 3 dirrefetn algorithms with the same purpose -
+    // - find a path in a network (graph)
+
+    // return a list of waypoints and recieve 2 waypoints
+    public static LinkedList<Waypoint> Breadthwise(Waypoint start, Waypoint end) {
+        return null;
+    }
+
+    public static LinkedList<Waypoint> Depthwise(Waypoint start, Waypoint end) {
+        return null;
+    }
+
+    public static LinkedList<Waypoint> AStar(Waypoint start, Waypoint end) {
+        return null;
+    }
+}
