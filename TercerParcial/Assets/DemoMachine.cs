3513754bff6242d3c7b420170872aaf92ccd6041
Fernando <A01630401@itesm.mx>
2019-04-05 12:40:35 -0600

    third partial first class

---
+++ TercerParcial/Assets/DemoMachine.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoMachine : MonoBehaviour
+{
+    private State currentState;
+    private State eating, playing, sleeping;
+    private Symbol getHungry, getsDark, getsBall;
+    private MonoBehaviour currentBehaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // language
+        getHungry = new Symbol("Get Hungry");
+        getsDark = new Symbol("Gets Dark");
+        getsBall = new Symbol("Gets Ball");
+
+        // states
+        eating = new State("EATING", typeof(EatingBehaviour));
+        playing = new State("PLAYING", typeof(PlayingBehaviour));
+        sleeping = new State("SLEEPING", typeof(SleepingBehaviour));
+
+        //transition function
+        eating.AddTransition(getHungry, eating);
+        eating.AddTransition(getsDark, sleeping);
+        eating.AddTransition(getsBall, playing);
+
+        playing.AddTransition(getHungry, playing);
+        playing.AddTransition(getsDark, sleeping);
+        playing.AddTransition(getsBall, playing);
+
+        sleeping.AddTransition(getHungry, sleeping);
+        sleeping.AddTransition(getsDark, eating);
+        sleeping.AddTransition(getsBall, eating);
+
+        // initial state
+        currentState = playing;
+        currentBehaviour = gameObject.AddComponent(currentState.Behaviour) as MonoBehaviour;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(currentState.Name);
+
+        if (Input.GetKeyUp(KeyCode.H)) {
+            currentState = currentState.ApplySymbol(getHungry);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D)) {
+            currentState = currentState.ApplySymbol(getsDark);
+        }
+
+        if (Input.GetKeyUp(KeyCode.B)) {
+            currentState = currentState.ApplySymbol(getsBall);
+        }
+    }
+}
---
+++ TercerParcial/Assets/State.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class State
+{
+    private string name;
+    private Dictionary<Symbol, State> transition;
+    private Type behaviour;
+
+    public State(string name, Type behaviour) {
+        this.name = name;
+        this.behaviour = behaviour;
+        transition = new Dictionary<Symbol, State>();
+    }
+
+    public Type Behaviour {
+        get {
+            return behaviour;
+        }
+    }
+
+    public string Name {
+        get {
+            return name;
+        }
+    }
+
+    public Dictionary<Symbol, State> Transition {
+        get {
+            return transition;
+        }
+    }
+
+    public void AddTransition(Symbol symbol, State state) {
+        transition.Add(symbol, state);
+    }
+
+    public State ApplySymbol(Symbol symbol) {
+
+        if (!transition.ContainsKey(symbol)) {
+            return this;
+        }
+
+        return transition[symbol];
+    }
+}
